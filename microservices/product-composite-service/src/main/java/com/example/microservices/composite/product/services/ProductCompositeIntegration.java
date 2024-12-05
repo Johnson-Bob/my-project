e6251dbb3c474abc2cbdbac77d7ea3c24ceed92b
Udovenko <>
2024-12-05 11:47:28 +0200

    add Spring Gateway

--- microservices/product-composite-service/src/main/java/com/example/microservices/composite/product/services/ProductCompositeIntegration.java
+++ microservices/product-composite-service/src/main/java/com/example/microservices/composite/product/services/ProductCompositeIntegration.java
@@ -14,7 +14,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.actuate.health.Health;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.HttpStatus;
 import org.springframework.messaging.Message;
@@ -121,26 +120,6 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
   public Mono<Void> deleteReviews(int productId) {
     return Mono.fromRunnable(() -> sendMessage("reviews-out-0", new Event<>(DELETE, productId, null)))
         .subscribeOn(publishEventScheduler).then();
-  }
-
-  public Mono<Health> getProductHealth() {
-    return getHealth(PRODUCT_SERVICE_URL);
-  }
-  public Mono<Health> getRecommendationHealth() {
-    return getHealth(RECOMMENDATION_SERVICE_URL);
-  }
-  public Mono<Health> getReviewHealth() {
-    return getHealth(REVIEW_SERVICE_URL);
-  }
-
-  private Mono<Health> getHealth(String url) {
-    url += "/actuator/health";
-    log.debug("Will call the Health API on URL: {}", url);
-    return webClient.get().uri(url).retrieve().bodyToMono(String.class)
-        .map(s -> new Health.Builder().up().build())
-        .onErrorResume(ex -> Mono.just(new
-            Health.Builder().down(ex).build()))
-        .log(log.getName(), FINE);
   }
 
   private RuntimeException handleException(WebClientResponseException ex) {
